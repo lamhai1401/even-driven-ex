1b7e759d56a79534987f569e506c9609b0f38134
lamhai1401 <lamthanhhai141@gmail.com>
2022-08-19 21:10:15 +0700

    add nats component

---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/lamhai1401/even-driven-ex
+
+go 1.19
+
+require (
+	contrib.go.opencensus.io/exporter/zipkin v0.1.2
+	github.com/go-kit/kit v0.12.0
+	github.com/nats-io/nats.go v1.16.0
+	github.com/openzipkin/zipkin-go v0.4.0
+	go.opencensus.io v0.23.0
+)
+
+require (
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/nats-io/nats-server/v2 v2.8.4 // indirect
+	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20220111092808-5a964db01320 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210917145530-b395a37504d4 // indirect
+	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ pkg/natsutil/component.go
@@ -0,0 +1,66 @@
+package natsutil
+
+import (
+	"sync"
+
+	"github.com/nats-io/nats.go"
+)
+
+// NATSComponent is contains reusable logic related to handling
+// of the connection to NATS  in the system.
+type NATSComponent struct {
+	// cmu is the lock from the component.
+	mutex sync.Mutex
+
+	// nc is the connection to NATS Streaming.
+	nc *nats.Conn
+
+	// name is the name of component.
+	name string
+}
+
+// NewNATSComponent creates a StreamingComponent
+func NewNATSComponent(name string) *NATSComponent {
+	return &NATSComponent{
+		name: name,
+	}
+}
+
+// ConnectToServer connects to NATS Server
+func (c *NATSComponent) ConnectToServer(url string, options ...nats.Option) error {
+	// close old connection
+	c.Shutdown()
+
+	// Connect to NATS with Cluster Id, Client Id and customized options.
+	nc, err := nats.Connect(url, options...)
+	if err != nil {
+		return err
+	}
+	// set nats conn
+	c.mutex.Lock()
+	c.nc = nc
+	c.mutex.Unlock()
+	return nil
+}
+
+// NATS returns the current NATS connection.
+func (c *NATSComponent) NATS() *nats.Conn {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	return c.nc
+}
+
+// Name is the label used to identify the NATS connection.
+func (c *NATSComponent) Name() string {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	return c.name
+}
+
+// Shutdown makes the component go away.
+func (c *NATSComponent) Shutdown() error {
+	if c.NATS() != nil {
+		c.NATS().Close()
+	}
+	return nil
+}
